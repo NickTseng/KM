6526417b0502711c39a04667a5e7670b6702db36
Nick <nick@Nick.local>
2014-11-26 19:19:10 +0800

    root is blogs/index

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   get 'tags/:tag', to: 'posts#index', as: :tag
 
-  root 'pages#index'
+  root 'blogs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
