cf976597d3a61f36881414c4b7230981d902fe13
Jimmy <jimmy.lin@drsays.org>
2014-12-03 20:27:27 +0800

    fix ransack and acts-as-taggable-on 整合的問題

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,11 @@
 class BlogsController < ApplicationController
 
 	def index
+		  @q = Post.search(params[:q])
 		if params[:tag]
-			@posts = Post.tagged_with(params[:tag])
+		  @posts = Post.tagged_with(params[:tag])
 		else
-			# @posts = Post.all
-      @q = Post.search(params[:q])
-      @posts = @q.result(distinct: true)
+	      @posts = @q.result(distinct: true)
 		end
 	end
 end
